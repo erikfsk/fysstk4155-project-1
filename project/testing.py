19b27ad2240fbc7c6ad61a85c91c2c5018f1a8e6
Unknown <erikfsk@student.matnat.uio.no>
2018-10-05 17:33:04 +0200

    Litt random shit

--- project/testing.py
+++ project/testing.py
@@ -1,78 +1,24 @@
-from numpy import *
-
-a = 2
-b = a
-a = 3
-print(b)
-
-a_list = [1]
-a_array = ones(1)
-
-print(a_array,a_list)
-
-a_list_var = a_list[0]
-a_array_var = a_array[0]
-
-a_list_var = 2
-a_array_var = 2
-
-
-print(a_array,a_list)
-
 def test(tekst):
-	dict_bokstaver_til_tall = {
-	"a":2 ,
-	"b":2 ,
-	"c":2 ,
-	"d":3 ,
-	"e":3 ,
-	"f":3 ,
-	"g":4 ,
-	"h":4 ,
-	"i":4 ,
-	"j":5 ,
-	"k":5 ,
-	"l":5 ,
-	"m":6 ,
-	"n":6 ,
-	"o":6 ,
-	"p":7 ,
-	"q":7 ,
-	"r":7 ,
-	"s":7 ,
-	"t":8 ,
-	"u":8 ,
-	"v":8 ,
-	"w":9 ,
-	"x":9 ,
-	"y":9 ,
-	"z":9,
-	" ":" "}
+	dict_bokstaver_til_tall = {"a":2 ,"b":2 ,"c":2 ,"d":3 ,"e":3 ,"f":3 ,"g":4 ,"h":4 ,\
+								"i":4 ,"j":5 ,"k":5 ,"l":5 ,"m":6 ,"n":6 ,"o":6 ,\
+								"p":7 ,"q":7 ,"r":7 ,"s":7 ,"t":8 ,"u":8 ,"v":8 ,\
+								"w":9 ,"x":9 ,"y":9 ,"z":9," ":" "}
+	ny_tekst = ""
 	for i in tekst:
-		print(dict_bokstaver_til_tall[i],end="")
+		ny_tekst += str(dict_bokstaver_til_tall[i])
+	return ny_tekst
 
-test("mikael kiste")
+print(test("mikael kiste"))
 
 
+def test_injektiv(tekst):
+	dict_bokstaver_til_tall = {"a":2 ,"b":22 ,"c":222 ,"d":3 ,"e":33 ,"f":333 ,"g":4 ,"h":44 ,\
+								"i":444 ,"j":5 ,"k":55 ,"l":555 ,"m":6 ,"n":66 ,"o":666 ,\
+								"p":7 ,"q":77 ,"r":777 ,"s":7777 ,"t":8 ,"u":88 ,"v":888 ,\
+								"w":9 ,"x":99 ,"y":999 ,"z":9999," ":" "}
+	ny_tekst = ""
+	for i in tekst:
+		ny_tekst += str(dict_bokstaver_til_tall[i])
+	return ny_tekst
 
-
-a2 = [0.0025,
-0.0288,
-0.0013,
-0.0011]
-1e-05 2
-0.0036
-0.0588
-0.0019
-0.0017
-1e-05 3
-0.0054
-0.0880
-0.0032
-0.0033
-1e-05 4
-0.0078
-0.1196
-0.0049
-0.0046
-1e-05 5
+print(test_injektiv("mikael kiste"))
